7b421444fc98cd17e371dc668360e54a5371e8bf
jean <jean.leal@zup.com.br>
2020-04-08 19:45:16 -0300

    sequelize added

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost:5432',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
