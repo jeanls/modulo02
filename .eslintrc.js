c27671a5d7e1a69e5d043886328f5275a29a104e
jean <jean.leal@zup.com.br>
2020-04-08 19:31:07 -0300

    eslint e prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "linebreak-style": ["error", "windows"],
+    "camelcase": "off"
+    // "no-unsed-vars": ["error", {"argsIgnorePattern": "next"}]
+  },
+};
